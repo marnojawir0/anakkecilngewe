3a03fc1600540335a84dfb728c64b0ae6bb7c1cf
marnojawir0 <marnojawir0@gmail.com>
2025-07-09 23:33:41 +0700

    Add files via upload

--- src/utils/site.js
+++ src/utils/site.js
@@ -1,9 +1,9 @@
-export const url = 'https://bocilmaininmemek.pages.dev';
-export const nama = 'Bocil Mainin Memek';
+export const url = 'https://anakkecilngewe.pages.dev';
+export const nama = 'Anak Kecil Ngewe';
 export const desk = 'Video Bokep Indo Terbaru Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante';
-export const terbit = '2025-03-09T00:01:03Z';
+export const terbit = '2025-01-25T00:01:03Z';
 
-export const google = '7moXEYuERwVUxxdiEQzxoI_ibGrKzivjocyjLMd0R6E';
-export const yandex = '708d9909d9d9ea76';
+export const google = 'excBslNNKnj8iVXDVGV6WCxPRz5ISB1B4vxEq8WldnM';
+export const yandex = 'd078362b6ed27a7e';
 
-export const index = 'c136a1a4-d047-446b-843e-4227cb5077b4';
+export const index = 'e7291702-31fa-4056-95c0-8540b88098a8';
